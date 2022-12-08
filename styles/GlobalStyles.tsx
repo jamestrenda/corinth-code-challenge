55adc1498df835cc6d0ba6de8b2a32a0bfcac977
James Trenda <jamestrenda@gmail.com>
2022-12-08 01:34:11 -0500

    add markup and initial styling

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -3,7 +3,7 @@ import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro';
 
 const CustomStyles = createGlobalStyle({
   body: {
-    ...tw`antialiased`,
+    ...tw`antialiased p-4 bg-gray-900`,
   },
 });
 
