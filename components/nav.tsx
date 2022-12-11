1a665832ec9623225960049f8438a12880172097
James Trenda <jamestrenda@gmail.com>
2022-12-11 02:29:35 -0500

    add active links

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,17 +1,20 @@
-import Link from 'next/link';
-import tw from 'twin.macro';
+import tw, { styled } from 'twin.macro';
 import { NavLink } from './navLink';
 import { getRomanNumeral } from '../utils/getRomanNumeral';
 import { fetchFilms } from 'lib/fetchFilms';
 import { useEffect, useState } from 'react';
-
-// TODO: add active link
-// TODO: type checking
+import ActiveLink from './activeLink';
 
 type Paths = {
   id: string;
   roman: string;
 };
+
+const StyledActiveLink = styled(ActiveLink)`
+  &.active {
+    ${tw`text-yellow-400`},
+  }
+`;
 
 export const Nav: React.FC = () => {
   const [paths, setPaths] = useState<Paths[] | undefined>(undefined);
@@ -42,7 +45,12 @@ export const Nav: React.FC = () => {
           ?.sort((a, b) => Number(a.id) - Number(b.id))
           .map(({ id, roman }) => (
             <NavLink key={roman}>
-              <Link href={`/episodes/${id}`}>{roman.toUpperCase()}</Link>
+              <StyledActiveLink
+                activeClassName="active"
+                href={`/episodes/${id}`}
+              >
+                {roman.toUpperCase()}
+              </StyledActiveLink>
             </NavLink>
           ))}
       </ul>
