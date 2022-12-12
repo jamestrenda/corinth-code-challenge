e5839f20ba971e98374beb47733584363b72f6c2
James Trenda <jamestrenda@gmail.com>
2022-12-12 11:10:01 -0500

    fetch all pages

--- lib/fetchSpecies.ts
+++ lib/fetchSpecies.ts
@@ -1,9 +1,37 @@
+import { IResource } from 'types/swapi';
+
 export type Species = {
   name: string;
   url: string;
 };
-export const fetchSpecies = async (endpoint: string): Promise<Species> => {
-  const res = await fetch(endpoint);
-  const data = await res.json();
-  return data as Species;
+interface ISpeciesResource extends IResource {
+  results: Species[];
+}
+export const fetchSpecies = async (
+  endpoint: string
+): Promise<ISpeciesResource> => {
+  // setup an array to store the people from each request
+  let results: Species[] = [];
+
+  const fetchMore = async (
+    endpoint = 'https://swapi.dev/api/species/'
+  ): Promise<ISpeciesResource> => {
+    try {
+      const res = await fetch(endpoint);
+      const data = await res.json();
+
+      results.push(...data.results);
+
+      // store the results
+      // make additional api calls for each remaining page (api limits response to 10 results each)
+      return !data.next
+        ? ({ ...data, results } as ISpeciesResource)
+        : fetchMore(data.next);
+    } catch (error) {
+      console.error(error);
+      return { count: 0, next: null, previous: null, results: [] };
+    }
+  };
+
+  return fetchMore();
 };
--- lib/fetchStarships.ts
+++ lib/fetchStarships.ts
@@ -1,9 +1,37 @@
-export type Starship = {
+import { IResource } from 'types/swapi';
+
+export type Starships = {
   name: string;
   url: string;
 };
-export const fetchStarships = async (endpoint: string): Promise<Starship> => {
-  const res = await fetch(endpoint);
-  const data = await res.json();
-  return data as Starship;
+interface IStarshipsResource extends IResource {
+  results: Starships[];
+}
+export const fetchSpecies = async (
+  endpoint: string
+): Promise<IStarshipsResource> => {
+  // setup an array to store the people from each request
+  let results: Starships[] = [];
+
+  const fetchMore = async (
+    endpoint = 'https://swapi.dev/api/starships/'
+  ): Promise<IStarshipsResource> => {
+    try {
+      const res = await fetch(endpoint);
+      const data = await res.json();
+
+      results.push(...data.results);
+
+      // store the results
+      // make additional api calls for each remaining page (api limits response to 10 results each)
+      return !data.next
+        ? ({ ...data, results } as IStarshipsResource)
+        : fetchMore(data.next);
+    } catch (error) {
+      console.error(error);
+      return { count: 0, next: null, previous: null, results: [] };
+    }
+  };
+
+  return fetchMore();
 };
