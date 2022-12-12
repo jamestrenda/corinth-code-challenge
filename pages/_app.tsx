2143e72a52647f07d23576cf59010e07bf14c816
James Trenda <jamestrenda@gmail.com>
2022-12-12 17:35:03 -0500

    add lightsaber-ish progress bar for route changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import GlobalStyles from '@/styles/GlobalStyles';
 import { Layout } from '@/components/layout';
 import { AppProvider } from '@/components/appStateProvider';
 import Head from 'next/head';
 import ErrorBoundary from '@/components/error';
+import '../styles/nprogress.css';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+
+NProgress.configure({ showSpinner: false });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => NProgress.start());
+    router.events.on('routeChangeComplete', () => NProgress.done());
+    router.events.on('routeChangeError', () => NProgress.done());
+  }, []);
+
   return (
     <>
       <Head>
