40ffbd8c8bff77fa3396432a47e6a93926ac971d
James Trenda <jamestrenda@gmail.com>
2022-12-11 20:27:22 -0500

    reduce number of api fetches needed

--- lib/fetchPeople.ts
+++ lib/fetchPeople.ts
@@ -1,5 +1,6 @@
 import { Person } from '@/components/profile';
 import { IResource } from 'types/swapi';
+import { Film } from './fetchFilms';
 interface IPersonResource extends IResource {
   results: Person[];
 }
@@ -7,6 +8,11 @@ interface IPersonResource extends IResource {
 export const fetchPeople = async (): Promise<IPersonResource> => {
   // setup an array to store the people from each request
   let results: Person[] = [];
+
+  // store fetched films, species, and starships to avoid making duplicate requests
+  const films = new Map();
+  const species = new Map();
+  const starships = new Map();
 
   // fetch all people
   const fetchMore = async (
@@ -20,40 +26,72 @@ export const fetchPeople = async (): Promise<IPersonResource> => {
       data.results.map(async (person: Person) => {
         // fetch film data
         const filmData = await Promise.all(
-          person.films.map(async (film) => {
-            const res = await fetch(String(film));
-            return await res.json();
+          person.films.map(async (f) => {
+            // check if film data has already been fetched and stored
+            if (!films.has(f)) {
+              // if not, fetch the film data and convert to json
+              const res = await fetch(String(f));
+              const data = await res.json();
+
+              const film = {
+                episode_id: data.episode_id,
+                title: data.title,
+                url: data.url,
+              };
+
+              // store the film data for subsequent requests
+              films.set(f, film);
+
+              // return the film data used to update the person object
+              return film;
+            }
+            // if film data has already been fetched, return the stored data
+            return films.get(f);
           })
-        ).then((data) => {
-          return data.map(({ episode_id, title, url }) => ({
-            episode_id,
-            title,
-            url,
-          }));
-        });
+        );
 
         // fetch species data
-        const species = await Promise.all(
+        const speciesData = await Promise.all(
           person.species.map(async (speciesUrl) => {
-            const res = await fetch(speciesUrl);
-            return await res.json();
+            // check if species data has already been fetched and stored
+            if (!species.has(speciesUrl)) {
+              // if not, fetch the species data and convert to json
+              const res = await fetch(speciesUrl);
+              const data = await res.json();
+              // store the species data for subsequent requests
+              species.set(speciesUrl, data.name);
+              // return the species data used to update the person object
+              return data.name;
+            }
+            // if species data has already been fetched, return the stored data
+            return species.get(speciesUrl);
           })
-        ).then((response) => response.map((data) => data.name));
+        );
 
         // fetch starship data
-        const starships = await Promise.all(
+        const starshipsData = await Promise.all(
           person.starships.map(async (starshipUrl) => {
-            const res = await fetch(starshipUrl);
-            return await res.json();
+            // check if starship data has already been fetched and stored
+            if (!starships.has(starshipUrl)) {
+              // if not, fetch the starship data and convert to json
+              const res = await fetch(starshipUrl);
+              const data = await res.json();
+              // store the starship data for subsequent requests
+              starships.set(starshipUrl, data.name);
+              // return the starship data used to update the person object
+              return data.name;
+            }
+            // if starship data has already been fetched, return the stored data
+            return starships.get(starshipUrl);
           })
-        ).then((data) => data.map((data) => data.name).join(', '));
+        ).then((data) => data.join(', '));
 
         // return the person with the updated data
         return {
           ...person,
           filmData,
-          species,
-          starships,
+          species: speciesData,
+          starships: starshipsData,
         };
       })
     );
