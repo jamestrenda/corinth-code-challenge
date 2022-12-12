a5dd36b52b5b43c294d0e0466f6e7c4272371208
James Trenda <jamestrenda@gmail.com>
2022-12-11 23:40:01 -0500

    add custom document to fix FOUT

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+// This code prevents a Flash Of Unstyled Content (FOUC)
+// on load. Without it, the styles are only added once
+// react loads on the frontend
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key="styles">
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
