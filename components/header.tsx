98258698b8c121183368e2627609ffd602be214d
James Trenda <jamestrenda@gmail.com>
2022-12-14 13:49:17 -0500

    add "priority" prop for above fold image

--- components/header.tsx
+++ components/header.tsx
@@ -10,6 +10,7 @@ export const Header: React.FC = () => {
       <h1 tw="text-yellow-400 font-bold text-2xl uppercase tracking-widest text-center mt-12">
         <Link href="/" tw="max-w-sm block m-auto">
           <Image
+            priority
             src="/StarWarsOpeningLogo.svg"
             width="1920"
             height="1080"
