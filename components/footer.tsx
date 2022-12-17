38b6f5657b80eb108b048a4af2609f87f925e2af
James Trenda <jamestrenda@gmail.com>
2022-12-17 17:49:50 -0500

    fade in quote

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { AppContext } from './appStateProvider';
+import { motion } from 'framer-motion';
 import tw from 'twin.macro';
 
 export const Footer: React.FC = () => {
@@ -9,12 +10,16 @@ export const Footer: React.FC = () => {
   return (
     <footer>
       {characters.length && (
-        <figure tw="text-3xl font-serif italic text-gray-500 text-center my-32">
+        <motion.figure
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          tw="text-3xl font-serif italic text-gray-500 text-center my-32"
+        >
           <blockquote tw="mb-2">
             <p>"I find your lack of faith disturbing."</p>
           </blockquote>
           <figcaption>&mdash; Darth Vader</figcaption>
-        </figure>
+        </motion.figure>
       )}
     </footer>
   );
