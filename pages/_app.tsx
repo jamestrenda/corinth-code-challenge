ffb9b326d7d45fdf9675f7dfb2f3ee2e6bbec3f6
James Trenda <jamestrenda@gmail.com>
2022-12-17 17:39:43 -0500

    remove maximum-scale attr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,10 +25,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Star Wars Character Profiles</title>
         <link rel="shortcut icon" href="/favicon.ico" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
           content="A Next.js site for querying Star Wars character data."
