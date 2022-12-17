1965797929cb014a55de1144b33875a5f4412416
James Trenda <jamestrenda@gmail.com>
2022-12-17 17:13:47 -0500

    remove dynamic call to swapi

--- components/nav.tsx
+++ components/nav.tsx
@@ -42,29 +42,12 @@ export const Nav: React.FC = () => {
   return (
     <nav>
       <ul tw="flex gap-8 font-bold text-gray-500 justify-center items-center uppercase">
-        {paths ? (
-          paths
-            .sort((a, b) => Number(a.id) - Number(b.id))
-            .map(({ id, roman }) => (
-              <NavLink key={roman}>
-                <StyledActiveLink
-                  activeClassName="active"
-                  href={`/episodes/${id}`}
-                >
-                  {roman}
-                </StyledActiveLink>
-              </NavLink>
-            ))
-        ) : (
-          <>
-            <li tw="h-12 grid place-items-center animate-pulse">i</li>
-            <li tw="h-12 grid place-items-center animate-pulse">ii</li>
-            <li tw="h-12 grid place-items-center animate-pulse">iii</li>
-            <li tw="h-12 grid place-items-center animate-pulse">iv</li>
-            <li tw="h-12 grid place-items-center animate-pulse">v</li>
-            <li tw="h-12 grid place-items-center animate-pulse">vi</li>
-          </>
-        )}
+        <li tw="h-12 grid place-items-center">i</li>
+        <li tw="h-12 grid place-items-center">ii</li>
+        <li tw="h-12 grid place-items-center">iii</li>
+        <li tw="h-12 grid place-items-center">iv</li>
+        <li tw="h-12 grid place-items-center">v</li>
+        <li tw="h-12 grid place-items-center">vi</li>
       </ul>
     </nav>
   );
