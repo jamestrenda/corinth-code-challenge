2d987b4c4b2f1a61a5932a4a997a5affaa51ac2a
James Trenda <jamestrenda@gmail.com>
2022-12-17 22:16:21 -0500

    remove unseen animation

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 import { AppContext } from './appStateProvider';
-import { motion } from 'framer-motion';
 import tw from 'twin.macro';
 
 export const Footer: React.FC = () => {
@@ -10,16 +9,12 @@ export const Footer: React.FC = () => {
   return (
     <footer>
       {characters.length && (
-        <motion.figure
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          tw="text-3xl font-serif italic text-gray-500 text-center my-32"
-        >
+        <figure tw="text-3xl font-serif italic text-gray-500 text-center my-32">
           <blockquote tw="mb-2">
             <p>"I find your lack of faith disturbing."</p>
           </blockquote>
           <figcaption>&mdash; Darth Vader</figcaption>
-        </motion.figure>
+        </figure>
       )}
     </footer>
   );
