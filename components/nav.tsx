e4fcd5ef18cdbc8412a4b2d1b11bbff501ba5f58
James Trenda <jamestrenda@gmail.com>
2022-12-17 19:48:18 -0500

    remove call to fetchFilms

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,5 +1,4 @@
 import tw, { styled } from 'twin.macro';
-import { NavLink } from './navLink';
 import { getRomanNumeral } from '../utils/getRomanNumeral';
 import { fetchFilms } from 'lib/fetchFilms';
 import { useEffect, useState } from 'react';
@@ -17,28 +16,6 @@ const StyledActiveLink = styled(ActiveLink)`
 `;
 
 export const Nav: React.FC = () => {
-  const [paths, setPaths] = useState<Paths[] | undefined>(undefined);
-
-  useEffect(() => {
-    // when this component mounts, dynamically fetch films to set as paths for the nav
-    const fetchData = async () => {
-      const { results } = await fetchFilms();
-
-      // convert episode_id's to roman numerals
-      const paths = results.map((film) => ({
-        id: film.episode_id.toString(),
-        roman: getRomanNumeral(film.episode_id) || '',
-      }));
-
-      setPaths(paths);
-    };
-    try {
-      fetchData();
-    } catch (error) {
-      console.log({ error });
-    }
-  }, []);
-
   return (
     <nav>
       <ul tw="flex gap-8 font-bold text-gray-500 justify-center items-center uppercase">
