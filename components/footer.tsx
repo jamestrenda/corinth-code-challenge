b021a499c47b8b3046186b2d13e259363e44d2b1
James Trenda <jamestrenda@gmail.com>
2022-12-19 11:03:20 -0500

    remove code previously used to hack CLS

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,21 +1,14 @@
-import { useContext } from 'react';
-import { AppContext } from './appStateProvider';
 import tw from 'twin.macro';
 
 export const Footer: React.FC = () => {
-  const { state } = useContext(AppContext);
-  const { characters } = state;
-
   return (
     <footer>
-      {characters.length && (
-        <figure tw="text-3xl font-serif italic text-gray-500 text-center my-32">
-          <blockquote tw="mb-2">
-            <p>"I find your lack of faith disturbing."</p>
-          </blockquote>
-          <figcaption>&mdash; Darth Vader</figcaption>
-        </figure>
-      )}
+      <figure tw="text-3xl font-serif italic text-gray-500 text-center my-32">
+        <blockquote tw="mb-2">
+          <p>"I find your lack of faith disturbing."</p>
+        </blockquote>
+        <figcaption>&mdash; Darth Vader</figcaption>
+      </figure>
     </footer>
   );
 };
